6578d34db2c666797abff99d82efee8679680677
awalliayni <yunipalupy@gmail.com>
2021-09-20 20:02:07 +0700

    Upload awal project

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+  
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo"Nama saya Awallia Yuni Palupy saya tinggal di Gg.Besar
+            olahraga yang disukai adalah Bulu Tangkis"; 
+    }   
+}
